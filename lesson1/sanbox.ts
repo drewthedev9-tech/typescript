21d9422d067e19eb77a5d35f33a2f9902f861093
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-10-14 21:02:22 -0400

    first lesson compiling done

---
+++ lesson1/sanbox.ts
@@ -0,0 +1,9 @@
+
+// getting all the inputs from the 
+const input = document.querySelectorAll('input');
+
+console.log(input);
+
+input.forEach(input => {
+    console.log(input)
+}); //outputs the three forms named inout in index.html
